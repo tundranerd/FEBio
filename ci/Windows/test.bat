96a0aa4e7a4c921994536206fa8f3ddfdc8923ad
John Haynes <tundranerd@gmail.com>
2024-03-02 20:54:29 -0700

    Remove legacy test-suite code

--- ci/Windows/test.bat
+++ ci/Windows/test.bat
@@ -1,2 +1,3 @@
-call "%ONEAPI_ROOT%\setvars.bat" --force
-python ./TestSuite/code/tools.py -r ./cmbuild/bin/Release/febio4.exe
+set PYTHON="%ONEAPI_ROOT%intelpython\latest\python"
+copy "%ONEAPI_ROOT%\compiler\latest\windows\redist\intel64_win\compiler\libiomp5md.dll" cmbuild\bin\Release
+%PYTHON% ./TestSuite/code/tools.py -r ./cmbuild/bin/Release/febio4.exe
