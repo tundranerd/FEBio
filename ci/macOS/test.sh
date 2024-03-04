0b6ceb91438002885f5dfec031d1f2416348e2e8
John Haynes <tundranerd@gmail.com>
2024-03-04 10:10:21 -0700

    MacOS test setup and run

--- ci/macOS/test.sh
+++ ci/macOS/test.sh
@@ -1,7 +1,30 @@
 #! /bin/bash
 # Uncomment next line if not global on target machine
 set -e
-source "/opt/intel/oneapi/setvars.sh" --force
-ls -lr cmbuild
+
 chmod +x cmbuild/bin/febio4
-./TestSuite/code/tools.py -r ./cmbuild/bin/febio4
+
+FEBIO_XML="$(dirname $0)/febio.xml"
+FEBIO_DIR=./cmbuild/bin
+FEBIO_LIB=./cmbuild/lib
+FEBIO_BIN="${FEBIO_DIR}/febio4"
+
+FEBIOHEAT=./febioheat/lib/libFEBioHeat.dylib
+FEBIOCHEM=./febioheat/lib/libFEBioHeat.dylib
+
+TESTSUITE=./TestSuite
+
+if [[ ! -d $TESTSUITE ]]; then
+	echo "Error: TestSuite was not located" >&2; exit 1
+fi
+
+if [[ -f "$FEBIOHEAT" ]]; then
+    cp $FEBIOHEAT $FEBIO_LIB
+fi
+
+if [[ -f "$FEBIOCHEM" ]]; then
+    cp $FEBIOCHEM $FEBIO_LIB
+fi
+
+cp $FEBIO_XML $FEBIO_DIR
+./TestSuite/code/tools.py -r $FEBIO_BIN
